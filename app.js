35c75b5e76d8e5d102a118f94a171d1ae41a99c8
Amalia Zebi <amalia.zebi@b4finance.com>
2021-02-15 18:46:48 +0100

    Second Commit - changes over the second page with a little magnifier

--- app.js
+++ app.js
@@ -15,20 +15,13 @@ const questionDisplay =document.querySelector('.question');
 const option1 = document.querySelector("#option1");
 const option2 = document.querySelector("#option2");
 const option3 = document.querySelector("#option3");
+const progress = document.querySelector('.progress');
 
 
 var correctAnswers = 0;
 let question;
 
-function startGame(){
-    page1.style.visibility = "visible";
-    page2.style.visibility = "visible";
-    page3.style.visibility = "hidden";
-    correctAnswers = 0;
-    questionOne();
-  }
-// buttonStart.onclick="window.location.href='/page2";
-buttonStart.onclick = startGame();
+
 
 var questionOne = () => {
     question = {
@@ -37,13 +30,33 @@ var questionOne = () => {
     option1: "tatatta",
     option2: "tatatta",
     option3: "tatatta",
-    option4: "Jtatatta",
     correctAnswer: "TATATA"
     };
     displayQuestion(question);
     displayProgress(question.number);
     return question;
   };
+
+function startGame(){
+    page2.style.display = 'flex';
+    page2.style.visibility = 'visible';
+    questionDisplay.style.display = 'none';
+    option1.style.display = 'none';
+    option2.style.display = 'none';
+    option3.style.display = 'none';
+    page1.style.display = 'none';
+    page1.style.visibility = 'none';
+    progress.style.display = 'none';
+    correctAnswers = 0;
+    console.log("hello");
+    console.log("OK????");
+  }
+  buttonStart.onclick = startGame;
+  console.log(buttonStart);
+
+
+
+
   var questionTwo = () => {
     question = {
     number: "2",
@@ -51,7 +64,6 @@ var questionOne = () => {
     option1: "tatatta",
     option2: "tatatta",
     option3: "tatatta",
-    option4: "Jtatatta",
     correctAnswer: "TATATA"
     };
     displayQuestion(question);
@@ -65,7 +77,6 @@ var questionOne = () => {
     option1: "tatatta",
     option2: "tatatta",
     option3: "tatatta",
-    option4: "Jtatatta",
     correctAnswer: "TATATA"
     };
     displayQuestion(question);
@@ -79,7 +90,6 @@ var questionOne = () => {
     option1: "tatatta",
     option2: "tatatta",
     option3: "tatatta",
-    option4: "Jtatatta",
     correctAnswer: "TATATA"
     };
     displayQuestion(question);
@@ -93,7 +103,6 @@ var questionOne = () => {
     option1: "tatatta",
     option2: "tatatta",
     option3: "tatatta",
-    option4: "Jtatatta",
     correctAnswer: "TATATA"
     };
     displayQuestion(question);
@@ -107,7 +116,6 @@ var questionOne = () => {
     option1: "tatatta",
     option2: "tatatta",
     option3: "tatatta",
-    option4: "Jtatatta",
     correctAnswer: "TATATA"
     };
     displayQuestion(question);
@@ -121,7 +129,6 @@ var questionOne = () => {
     option1: "tatatta",
     option2: "tatatta",
     option3: "tatatta",
-    option4: "Jtatatta",
     correctAnswer: "TATATA"
     };
     displayQuestion(question);
@@ -135,7 +142,6 @@ var questionOne = () => {
     option1: "tatatta",
     option2: "tatatta",
     option3: "tatatta",
-    option4: "Jtatatta",
     correctAnswer: "TATATA"
     };
     displayQuestion(question);
@@ -149,7 +155,6 @@ var questionOne = () => {
     option1: "tatatta",
     option2: "tatatta",
     option3: "tatatta",
-    option4: "Jtatatta",
     correctAnswer: "TATATA"
     };
     displayQuestion(question);
@@ -163,7 +168,6 @@ var questionOne = () => {
     option1: "tatatta",
     option2: "tatatta",
     option3: "tatatta",
-    option4: "Jtatatta",
     correctAnswer: "TATATA"
     };
     displayQuestion(question);
@@ -192,7 +196,7 @@ var questionOne = () => {
   function showScore(correctAnswers){
     var score = document.getElementById("score");
     score.innerHTML = `You got ${correctAnswers} out of 10 dundies!`;
-    page1.style.visibility = "hidden";
+    page1.style.visibility = "visible";
     page2.style.visibility = "hidden";
     page3.style.visibility = "visible";
     gameOver.style.visibility = "visible";
@@ -200,8 +204,6 @@ var questionOne = () => {
     buttonStart.style.visibility = "visible";
   }
 
-  startGame();
-questionOne();
 
 //   function moveToNextQuestion(currentQuestion){
 //     switch (currentQuestion){
@@ -223,3 +225,60 @@ questionOne();
 //       default:
 //     }
 //   }
+
+//Magnifier Glass
+function magnify(imgID, zoom) {
+  var img, glass, w, h, bw;
+  img = document.getElementById(imgID);
+  /*create magnifier glass:*/
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "img-magnifier-glass");
+  /*insert magnifier glass:*/
+  img.parentElement.insertBefore(glass, img);
+  /*set background properties for the magnifier glass:*/
+  glass.style.backgroundImage = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+  /*execute a function when someone moves the magnifier glass over the image:*/
+  glass.addEventListener("mousemove", moveMagnifier);
+  img.addEventListener("mousemove", moveMagnifier);
+  /*and also for touch screens:*/
+  glass.addEventListener("touchmove", moveMagnifier);
+  img.addEventListener("touchmove", moveMagnifier);
+  function moveMagnifier(e) {
+    var pos, x, y;
+    /*prevent any other actions that may occur when moving over the image*/
+    e.preventDefault();
+    /*get the cursor's x and y positions:*/
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    /*prevent the magnifier glass from being positioned outside the image:*/
+    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+    if (x < w / zoom) {x = w / zoom;}
+    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+    if (y < h / zoom) {y = h / zoom;}
+    /*set the position of the magnifier glass:*/
+    glass.style.left = (x - w) + "px";
+    glass.style.top = (y - h) + "px";
+    /*display what the magnifier glass "sees":*/
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+  }
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    /*get the x and y positions of the image:*/
+    a = img.getBoundingClientRect();
+    /*calculate the cursor's x and y coordinates, relative to the image:*/
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /*consider any page scrolling:*/
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {x : x, y : y};
+  }
+}
+magnify("image-dundies", 2);
